e6d3d51fd7a5695b17d3d3cec0852fa0763141dd
thorcamara <thorcamaraalthoff@gmail.com>
2023-10-11 21:55:17 -0300

    Added more JS

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 const blurHeader = () => {
   const header = document.getElementById('header')
   this.scrollY >= 50 ? header.classList.add('blur-header')
-                    : header.classList.remove('blur-header')
+    : header.classList.remove('blur-header')
 }
 
 window.addEventListener('scroll', blurHeader)
@@ -33,6 +33,25 @@ window.addEventListener('scroll', blurHeader)
 const scrollUp = () => {
   const scrollUp = document.getElementById('scroll-up')
   this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-                      : scrollUp.classList.remove('show-scroll')
+    : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id'),
+      sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link')
+    } else {
+      sectionsClass.classList.remove('active-link')
+    }
+  })
+}
